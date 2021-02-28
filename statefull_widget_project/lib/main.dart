f836a1553f81491d1c574939caa675e3f5831a7d
dhikaainul <dhikaainul@gmail.com>
2021-02-28 12:57:46 +0700

    add suhu dalam kelvin dan reamur

--- statefull_widget_project/lib/main.dart
+++ statefull_widget_project/lib/main.dart
@@ -34,6 +34,47 @@ class MyApp extends StatelessWidget {
                   FilteringTextInputFormatter.digitsOnly
                 ],
               ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 240, 10, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.yellow[900]),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text("Suhu Dalam Kelvin"),
+                          SizedBox(height: 22),
+                          Text("150", style: TextStyle(fontSize: 45))
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.yellow[900]),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text("Suhu Dalam Reamur"),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "200",
+                            style: TextStyle(fontSize: 45),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
